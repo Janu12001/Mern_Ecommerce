621be73c652ac583911afb81c8417cc189a063f6
alonecoder122 <ak7616308@gmail.com>
2023-08-13 23:29:03 +0530

    controller user updated file

---
+++ controller/User.js
@@ -0,0 +1,24 @@
+const { User } = require("../model/User");
+
+//fetch Userbyid api
+exports.fetchUserById = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+//update user api
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
