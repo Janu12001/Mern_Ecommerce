bc0da85fc75dac92b717a3ea3f9302dd644991c5
alonecoder122 <ak7616308@gmail.com>
2023-08-16 22:56:12 +0530

    completed auth controller

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,12 +1,39 @@
 //create user api
 const { User } = require("../model/User");
-
+const crypto = require("crypto");
+const { sanitizeUser } = require("../services/common");
+const SECRET_KEY = "SECRET_KEY";
+const jwt = require("jsonwebtoken");
 //create user api
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
   try {
-    const doc = await user.save();
-    res.status(201).json({ id: doc.id, role: doc.role });
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(
+      req.body.password,
+      salt,
+      310000,
+      32,
+      "sha256",
+      async function (err, hashedPassword) {
+        const user = new User({ ...req.body, password: hashedPassword, salt });
+        const doc = await user.save();
+
+        req.login(sanitizeUser(doc), (err) => {
+          if (err) {
+            res.status(400).json(err);
+          } else {
+            const token = jwt.sign(sanitizeUser(doc), SECRET_KEY);
+            res
+              .cookie("jwt", token, {
+                expires: new Date(Date.now() + 3600000),
+                httpOnly: true,
+              })
+              .status(201)
+              .json({ id: doc.id, role: doc.role });
+          }
+        });
+      }
+    );
   } catch (err) {
     res.status(400).json(err);
   }
@@ -14,22 +41,21 @@ exports.createUser = async (req, res) => {
 
 //login user api
 exports.loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne(
-      { email: req.body.email },
-      "id name email"
-    ).exec();
-    // TODO: this is just temporary, we will use strong password auth
-    console.log({ user });
-    if (!user) {
-      res.status(401).json({ message: "no such user email" });
-    } else if (user.password === req.body.password) {
-      // TODO: We will make addresses independent of login
-      res.status(200).json({ id: user.id, role: user.role });
-    } else {
-      res.status(401).json({ message: "invalid credentials" });
-    }
-  } catch (err) {
-    res.status(400).json(err);
+  const user = req.user;
+  res
+    .cookie("jwt", user.token, {
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    })
+    .status(201)
+    .json({ id: user.id, role: user.role });
+};
+
+//check user
+exports.checkAuth = async (req, res) => {
+  if (req.user) {
+    res.json(req.user);
+  } else {
+    res.sendStatus(401);
   }
 };
